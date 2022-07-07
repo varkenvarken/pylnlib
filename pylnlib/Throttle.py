e6e19c42478b35c6e75c953162dfd592c1d90757
varkenvarken <michel@michelanders.nl>
2022-07-07 15:28:19 +0200

    doc tweaks

--- pylnlib/Throttle.py
+++ pylnlib/Throttle.py
@@ -4,17 +4,21 @@
 #
 # License: GPL 3, see file LICENSE
 #
-# Version: 20220706134653
+# Version: 20220707152752
 
 from threading import Timer
 
 
 class Throttle:
-    """
-    A class to control a single locomotive.
-    """
 
     def __init__(self, scrollkeeper, locaddress):
+        """
+        A class to control a single locomotive.
+
+        Args:
+            scrollkeeper (Scrollkeeper): used to send messages and retrieve slot information
+            locaddress (int): the address of the locomotive
+        """
         self.scrollkeeper = scrollkeeper
         self.locaddress = locaddress
 
@@ -23,11 +27,11 @@ class Throttle:
         Change the speed of a locomotive to forward and a given value.
 
         Args:
-            speed (float, optional): speed is a float [0.0, 1.0], setting it to zero will initiate an inertial stop. Defaults to 0.0.
+            speed (float, optional): speed is a float in the range [0.0, 1.0], setting it to zero will initiate an inertial stop. Defaults to 0.0.
 
         Two LocoNet messages may be generated:
-        - Only if the direction is changed, a LocoNet direction message is generated for the slot that controls this loco.
-        - Only if the speed is changed, a LocoNet speed message is generated for the slot that controls this loco.
+            - Only if the direction is changed, a LocoNet direction message is generated for the slot that controls this loco.
+            - Only if the speed is changed, a LocoNet speed message is generated for the slot that controls this loco.
         """
         slot = self.scrollkeeper.getSlot(self.locaddress)
         dirchanged = slot.dir != False
@@ -47,11 +51,11 @@ class Throttle:
         Change the speed of a locomotive to reverse and a given value.
 
         Args:
-            speed (float, optional): speed is a float [0.0, 1.0], setting it to zero will initiate an inertial stop. Defaults to 0.0.
+            speed (float, optional): speed is a float in the range [0.0, 1.0], setting it to zero will initiate an inertial stop. Defaults to 0.0.
         
         Two LocoNet messages may be generated:
-        - Only if the direction is changed, a LocoNet direction message is generated for the slot that controls this loco.
-        - Only if the speed is changed, a LocoNet speed message is generated for the slot that controls this loco.
+            - Only if the direction is changed, a LocoNet direction message is generated for the slot that controls this loco.
+            - Only if the speed is changed, a LocoNet speed message is generated for the slot that controls this loco.
         """        """"""
         slot = self.scrollkeeper.getSlot(self.locaddress)
         dirchanged = slot.dir != True
@@ -69,6 +73,13 @@ class Throttle:
             self.scrollkeeper.sendMessage(slot.speedMessage())
 
     def lights(self, on=True, duration=0):
+        """
+        Turn directional lights on or off.
+
+        Args:
+            on (bool, optional): new state of the directional lights. Defaults to True.
+            duration (int, optional): if larger than zero will revert the lights to the previous state after duration seconds. Defaults to 0.
+        """        
         slot = self.scrollkeeper.getSlot(self.locaddress)
         msg, imsg = slot.function(0, on, duration)
         self.scrollkeeper.sendMessage(msg)
@@ -76,6 +87,13 @@ class Throttle:
             Timer(duration, self.scrollkeeper.sendMessage, args=[imsg]).start()
 
     def sound(self, on=True, duration=0):
+        """
+        Turn sound on or off.
+
+        Args:
+            on (bool, optional): new state of the  sound. Defaults to True.
+            duration (int, optional): if larger than zero will revert the sound to the previous state after duration seconds. Defaults to 0.
+        """        
         slot = self.scrollkeeper.getSlot(self.locaddress)
         msg, imsg = slot.function(1, on, duration)
         self.scrollkeeper.sendMessage(msg)
@@ -83,6 +101,15 @@ class Throttle:
             Timer(duration, self.scrollkeeper.sendMessage, args=[imsg]).start()
 
     def whistle(self, on=True, duration=0.5):
+        """
+        Sound the whistle.
+
+        Args:
+            on (bool, optional): new state of the whistle. Defaults to True.
+            duration (float, optional): if larger than zero will revert the whistle to the previous state after duration seconds. Defaults to 0.5.
+
+        A loc decoder will typically use a short whistle sound but it still needs to be turned off explicitely by the throttle, therefore the function defaults to 0.5 seconds.
+        """        
         slot = self.scrollkeeper.getSlot(self.locaddress)
         msg, imsg = slot.function(2, on, duration)
         self.scrollkeeper.sendMessage(msg)
