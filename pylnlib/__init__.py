6ceab8948d207582452130ebdf836391eb817566
varkenvarken <michel@michelanders.nl>
2022-07-10 11:37:19 +0200

    documentation

--- pylnlib/__init__.py
+++ pylnlib/__init__.py
@@ -4,7 +4,24 @@
 #
 # License: GPL 3, see file LICENSE
 #
-# Version: 20220708155418
+# Version: 20220710113704
+
 """
-This package ...
+This package can be invoked as a module.
+
+Example:
+```
+python -m pylnlib
+```
+
+If invoked this way, it will start listening for data on a serial interface 
+and will report any incoming messages. Incoming messages related to unknown
+slots, switches or sensors will trigger outgoing status request messages.
+
+It can both capture and replay messages. For more info run it with the -- help option
+
+```
+python -m pylnlib --help
+```
+
 """
--- pylnlib/__main__.py
+++ pylnlib/__main__.py
@@ -4,27 +4,7 @@
 #
 # License: GPL 3, see file LICENSE
 #
-# Version: 20220710112411
-
-"""
-Module to allow pylnlib to be invoked as a module.
-
-Example:
-```
-python -m pylnlib
-```
-
-If invoked this way, it will start listening for data on a serial interface 
-and will report any incoming messages. Incoming messages related to unknown
-slots, switches or sensors will trigger outgoing status request messages.
-
-It can both capture and replay messages. For more info run it with the -- help option
-
-```
-python -m pylnlib --help
-```
-
-"""
+# Version: 20220710113627
 
 from threading import Thread
 from time import sleep
