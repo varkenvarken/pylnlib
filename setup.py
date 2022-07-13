c0de3d61416555ecd391a5abace997e8d99dfffd
varkenvarken <michel@michelanders.nl>
2022-07-13 15:59:47 +0200

    extras require

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pylnlib",
-    version="0.2.0",
+    version="0.2.1",
     description="A library to monitor LocoNet message on a serial interface",
     url="https://varkenvarken.github.io/pylnlib/",
     author="varkenvarken",
@@ -12,5 +12,6 @@ setup(
     install_requires=[
         "pyserial",
     ],
+    extras_require={"webserver": "fastapi[all]"},
     zip_safe=False,
 )
