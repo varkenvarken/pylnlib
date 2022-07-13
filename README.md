f466a750c44189db8dbe033c90ba6ca455d20911
varkenvarken <michel@michelanders.nl>
2022-07-13 16:22:35 +0200

    tweaked setup

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 ![Docs generated](https://github.com/varkenvarken/pylnlib/actions/workflows/black.yml/badge.svg)
-
+![Versions](https://img.shields.io/pypi/v/pylnlib)
+![Python versions](https://img.shields.io/pypi/pyversions/pylnlib)
 
 # pylnlib
 A python library to monitor LocoNet traffic on a usb/serial bus.
 
 Documentation (a work in progress) can be found on [https://varkenvarken.github.io/pylnlib/](https://varkenvarken.github.io/pylnlib/)
+
+# installation
+
+just the library:
+
+```bash
+pip install pylnlib
+```
+
+with the experimental webserver:
+
+```bash
+pip install pylnlib[webserver]
+```
--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="pylnlib",
     version="0.2.1",
     description="A library to monitor LocoNet message on a serial interface",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://varkenvarken.github.io/pylnlib/",
     author="varkenvarken",
     author_email="test@example.com",
     license="GPLv3",
     packages=["pylnlib"],
+    python_requires='>=3.8',
     install_requires=[
         "pyserial",
     ],
