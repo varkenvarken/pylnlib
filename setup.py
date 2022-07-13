c4a273b25a8107ac2b566509591b70d4a0733e91
varkenvarken <michel@michelanders.nl>
2022-07-13 16:37:58 +0200

    bumped release version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pylnlib",
-    version="0.2.2",
+    version="0.2.3",
     description="A library to monitor LocoNet message on a serial interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
