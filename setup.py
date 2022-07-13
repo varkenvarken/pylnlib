850fe24f944def878b82bb2b1704e3cf044e8e15
varkenvarken <michel@michelanders.nl>
2022-07-13 16:36:34 +0200

    added trove classifiers

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 from pathlib import Path
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
@@ -9,16 +10,23 @@ setup(
     version="0.2.2",
     description="A library to monitor LocoNet message on a serial interface",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://varkenvarken.github.io/pylnlib/",
     author="varkenvarken",
     author_email="test@example.com",
     license="GPLv3",
     packages=["pylnlib"],
-    python_requires='>=3.8',
+    python_requires=">=3.8",
     install_requires=[
         "pyserial",
     ],
     extras_require={"webserver": "fastapi[all]"},
     zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Framework :: FastAPI",
+    ],
 )
